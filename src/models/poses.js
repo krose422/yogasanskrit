a3217caf97c83a6fce05cb7478a7e64e8e51ab7e
Kelley Rose <kelleyrose@Kelleys-iMac.local>
2017-07-01 19:43:42 -0400

    Add yoga poses data file

---
+++ src/models/poses.js
@@ -0,0 +1,1276 @@
+/**
+* @providesModule PosesData
+*/
+
+import images from 'Images';
+
+const poses = [
+  {
+    id: 0,
+    sanskritName: 'Adho Mukha Svanasa',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 1,
+    sanskritName: 'Adho Mukha Vrksasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 2,
+    sanskritName: 'Ananda Balasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 3,
+    sanskritName: 'Ardha Anuvittasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 4,
+    sanskritName: 'Ardha Matsyendrasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 5,
+    sanskritName: 'Ardha Pincha Mayurasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 6,
+    sanskritName: 'Ardha Uttanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 7,
+    sanskritName: 'Ashtangasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 8,
+    sanskritName: 'Ashwa Sanchalanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 9,
+    sanskritName: 'Astavakrasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 10,
+    sanskritName: 'Baddha Konasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 11,
+    sanskritName: 'Baddha Parsvokanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 12,
+    sanskritName: 'Bakasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 13,
+    sanskritName: 'Balasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 14,
+    sanskritName: 'Bhuja Vrischikasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 15,
+    sanskritName: 'Bhujangasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 16,
+    sanskritName: 'Bitilasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 17,
+    sanskritName: 'Camatkarasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 18,
+    sanskritName: 'Chaturanga Dandasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 19,
+    sanskritName: 'Dandayamana Dhanurasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 20,
+    sanskritName: 'Dhanurasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 21,
+    sanskritName: 'Dwi Pada Viparita Dandasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 22,
+    sanskritName: 'Eka Pada Adho Mukha Svanasa',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 23,
+    sanskritName: 'Eka Pada Adho Mukha Svanasa',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 24,
+    sanskritName: 'Eka Pada Galavasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 25,
+    sanskritName: 'Eka Pada Raja Kapotasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 26,
+    sanskritName: 'Eka Pada Rajakapotasa',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 27,
+    sanskritName: 'Eka Pada Rajakapotasana 2',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 28,
+    sanskritName: 'Garudasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 29,
+    sanskritName: 'Gomukhasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 30,
+    sanskritName: 'Halasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 31,
+    sanskritName: 'Hanumanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 32,
+    sanskritName: 'Kapotasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 33,
+    sanskritName: 'Laghu Vajrasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 34,
+    sanskritName: 'Maksikanagasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 35,
+    sanskritName: 'Malasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 36,
+    sanskritName: 'Marichyasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 37,
+    sanskritName: 'Marjaryasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 38,
+    sanskritName: 'Mayurasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 39,
+    sanskritName: 'Natarajasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 40,
+    sanskritName: 'Padmasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 41,
+    sanskritName: 'Paripurna Navasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 42,
+    sanskritName: 'Parivrtta Parsvakonasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 43,
+    sanskritName: 'Parivrtta Surya Yantrasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 44,
+    sanskritName: 'Parivrtta Surya Trikonasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 45,
+    sanskritName: 'Parivrtta Ustrasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 46,
+    sanskritName: 'Parivrtta Utkatasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 47,
+    sanskritName: 'Parsva Bakasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 48,
+    sanskritName: 'Pincha Mayurasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 49,
+    sanskritName: 'Purvottanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 50,
+    sanskritName: 'Salabhasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 51,
+    sanskritName: 'Salamba Bhujangasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 52,
+    sanskritName: 'Salamba Sarvangasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 53,
+    sanskritName: 'Setu Bandha Sarvangasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 54,
+    sanskritName: 'Sirsasana 2',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 55,
+    sanskritName: 'Svarga Dvidasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 56,
+    sanskritName: 'Tadasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 57,
+    sanskritName: 'Tadasana 2',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 58,
+    sanskritName: 'Tittibhasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 59,
+    sanskritName: 'Urdha Dhanurasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 60,
+    sanskritName: 'Urdha Mukha Svanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 61,
+    sanskritName: 'Utkatasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 62,
+    sanskritName: 'Utrasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 63,
+    sanskritName: 'Uttana Shishosana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 64,
+    sanskritName: 'Uttanasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 65,
+    sanskritName: 'Utthan Pristhasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 66,
+    sanskritName: 'Utthita Parsvakonasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 67,
+    sanskritName: 'Utthita Trikonasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 68,
+    sanskritName: 'Uttihita Chaturanga Dandasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 69,
+    sanskritName: 'Vasisthasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 70,
+    sanskritName: 'Viparita Virabhadrasana 2',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 71,
+    sanskritName: 'Virabhadrasana 1',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 72,
+    sanskritName: 'Virabhadrasana 2',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 73,
+    sanskritName: 'Virabhadrasana 3',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 74,
+    sanskritName: 'Vrikshasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  },
+  {
+    id: 75,
+    sanskritName: 'Vrischikasana',
+    englishName: '',
+    imageUrl: '',
+    description: '',
+    tips: [
+      {
+        id: 0,
+        tip: ''
+      },
+      {
+        id: 1,
+        tip: ''
+      }
+    ]
+  }
+]
